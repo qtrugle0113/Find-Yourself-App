983d2f2fa7f22de6b377ff5aa2f524c66fb4dd11
qtrugle0113 <rikateamovt1@gmail.com>
2023-03-16 07:54:46 +0900

    added QnA Screen

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import kivy
 from kivy.app import App
 from kivy.config import Config
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
 
 Config.set('graphics', 'width', '450')
@@ -19,6 +20,24 @@ class WindowManager(ScreenManager):
 
 class MainWindow(Screen):
     pass
+
+
+
+class QnAWindow(Screen):
+    mood = StringProperty('Mood')
+
+    def on_slider_value(self, widget):
+
+        if widget.value < 20:
+            self.mood = 'Sad'
+        elif widget.value < 40:
+            self.mood = 'Negative'
+        elif widget.value < 60:
+            self.mood = 'Normal'
+        elif widget.value < 80:
+            self.mood = 'Positive'
+        else:
+            self.mood = 'Happy'
 
 
 class HistoryWindow(Screen):
